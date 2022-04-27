9df5be42d4cf28085ee5252a8456e437c90dd427
root <yluo3421@gmail.com>
2022-04-27 10:50:44 -0400

    commit tree edb9358e7f9d5d281a3857450e3763779af99ea4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+//function superbowlWin(obj) {
+//    function isWin() {
+//        return obj.result === "W";
+//    } 
+//    return(obj.find(isWin));
+//}
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
